330b52d01e400ff4bfd24417085e7c07e77fa382
Furkan Söylemez <furkansoylemez@outlook.com>
2025-08-04 15:44:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# figma_to_code_light
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/figma_to_code_light/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.figma_to_code_light
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
