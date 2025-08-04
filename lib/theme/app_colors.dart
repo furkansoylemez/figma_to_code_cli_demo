68526dd10d54cf82c32356896b554d6de401855e
Furkan Söylemez <furkansoylemez@outlook.com>
2025-08-04 16:43:40 +0300

    Update app_colors.dart

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 class AppColors {
-  // Base colors
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF1D1D1D);
   static const Color blackWithHalfOpacity = Color.fromRGBO(29, 29, 29, 0.5);
