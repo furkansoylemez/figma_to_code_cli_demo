ee92f97f3cabb701ecd7f3c80e2ef61a9257611a
Furkan Söylemez <furkansoylemez@outlook.com>
2025-08-04 16:22:39 +0300

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: appTheme,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
